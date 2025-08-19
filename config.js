6763d348a7c90e1a0aaad41b8ed73998a91b6ebe
edessarps <edessarps@gmail.com>
2025-08-19 19:35:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Remplace par ton Client ID Web OAuth 2.0
 export const GOOGLE_CLIENT_ID = '240084222867-jtrid9bq2ihr05ma51htuis5tkueinsg.apps.googleusercontent.com';
 // Mets à true pour activer la connexion Google & API live
-export const USE_LIVE_API = false;
+export const USE_LIVE_API = true;
